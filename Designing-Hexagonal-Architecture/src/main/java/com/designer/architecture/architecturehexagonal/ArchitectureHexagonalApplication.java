df240e1b798d798a7e9fabc3440625a70bc4f78c
jose mendoza sanchez <jmendozarv@outlook.com>
2023-11-11 23:35:33 -0500

    add new features

---
+++ Designing-Hexagonal-Architecture/src/main/java/com/designer/architecture/architecturehexagonal/ArchitectureHexagonalApplication.java
@@ -0,0 +1,13 @@
+package com.designer.architecture.architecturehexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitectureHexagonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchitectureHexagonalApplication.class, args);
+	}
+
+}
---
+++ Designing-Hexagonal-Architecture/src/test/java/com/designer/architecture/architecturehexagonal/ArchitectureHexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.designer.architecture.architecturehexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchitectureHexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
